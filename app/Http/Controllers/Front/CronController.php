4f2df4174fd793e8582937f83f9745e05094b8ce
Hans Christian Nataniel <nataniel.hans@gmail.com>
2018-02-23 18:06:39 +0700

    Fix cron controller: 1. record user mining history using the data from 2 hours earlier 2. update the user balance 3. faster sum active mh with sql instead of using a foreach loop

--- app/Http/Controllers/Front/CronController.php
+++ app/Http/Controllers/Front/CronController.php
@@ -1,13 +1,13 @@
 <?php
 
-/*
-	Use Name Space Here
-*/
+/**
+ * Use Name Space Here
+ */
 namespace App\Http\Controllers\Front;
 
-/*
-	Call Model Here
-*/
+/**
+ * Call Model Here
+ */
 use App\Models\Setting;
 use App\Models\User;
 use App\Models\Avg;
@@ -15,18 +15,17 @@ use App\Models\Purchase;
 use App\Models\Mininghistory;
 use App\Models\Usermininghistory;
 
-/*
-	Call Mail file & mail facades
-*/
+/**
+ * Call Mail file & mail facades
+ */
 use App\Mail\Front\Cron;
 use App\Mail\Front\Cronfail;
 
 use Illuminate\Support\Facades\Mail;
 
-
-/*
-	Call Another Function  you want to use
-*/
+/**
+ * Call Another Function  you want to use
+ */
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Auth;
@@ -35,7 +34,6 @@ use URL;
 use Image;
 use Session;
 use DB;
-
 
 class CronController extends Controller
 {
@@ -47,28 +45,29 @@ class CronController extends Controller
 
 			$cloudsendlink = getData("https://api.nanopool.org/v1/eth/user/" . $setting->cloud_mining_walletid);
 			
-			/*
-				Pengecekkan status API
-			*/
+			/**
+			 * Pengecekkan status API
+			 */
 			if($cloudsendlink["status"] != false)
 			{
 				$cloudgetbalance = $cloudsendlink["data"]["balance"];
 
-				/*
-					Create new record of mining history
-				*/
+				/**
+				 * Create new record of mining history
+				 */
 				$mininghistory = new Mininghistory;
 				
 				$mininghistory->balance_api = $cloudgetbalance;
 
-				/*
-					Get last mining history
-				*/
+				/**
+				 * Get last mining history and 2nd last mining history to be shared to users
+				 */
 				$lastmininghistory = Mininghistory::orderBy('id', 'desc')->first();
+				$sharedmininghistory = Mininghistory::orderBy('id', 'desc')->skip(1)->first();
 
-				/*
-					Pengecekkan data pertama dari table Mininghistories
-				*/
+				/**
+				 * Pengecekkan data pertama dari table Mininghistories
+				 */
 				if($lastmininghistory != null)
 				{
 					if($cloudgetbalance < $lastmininghistory->balance_api)
@@ -98,9 +97,9 @@ class CronController extends Controller
 				}
 				$mininghistory->save();
 
-				/*
-					Create new User Mining History
-				*/
+				/**
+				 * Create new User Mining History
+				 */
 				$users = User::where('is_active', '=', true)->where('is_suspended', '=', false)->where(function($qr){
 					$qr->where('cloudminingmh', '!=', 0);
 					$qr->orWhere('cloudminingmh', '!=', null);	
@@ -110,17 +109,19 @@ class CronController extends Controller
 					foreach ($users as $user) {
 						$lastusermininghistory = Usermininghistory::where('user_id', '=', $user->id)->orderBy('id', 'desc')->first();
 
-						$usermhs = Usermh::where('user_id', '=', $user->id)->where('status', '=', 'Active')->where('is_active', '=', true)->get();
-						$uncountmh = 0;
-						foreach ($usermhs as $usermh)
-						{
-							if (strtotime($usermh->active_time) > date("Y-m-d H:i:s", strtotime('-1 hour')))
-							{
-								$uncountmh = $uncountmh + $usermh->mh;
-							}
-						}
+						$uncount_time = date("Y-m-d H:i:s", strtotime('-1 hour'));
 
-						$userminingselisih = $lastmininghistory->selisih * (($user->cloudminingmh - $uncountmh) / $setting->totalmh);
+						$usermh = Usermh::select(DB::raw('sum(mh) as total_mh'))->where('user_id', '=', $user->id)->where('status', '=', 'Active')->where('is_active', '=', true)->where('active_time', '>', $uncount_time)->first();
+						// $uncountmh = 0;
+						// foreach ($usermhs as $usermh)
+						// {
+						// 	if (strtotime($usermh->active_time) > date("Y-m-d H:i:s", strtotime('-1 hour')))
+						// 	{
+						// 		$uncountmh = $uncountmh + $usermh->mh;
+						// 	}
+						// }
+
+						$userminingselisih = $sharedmininghistory->selisih * ($usermh->total_mh / $setting->totalmh);
 
 						$usermininghistory = new Usermininghistory;
 						$usermininghistory->user_id = $user->id;
@@ -135,12 +136,14 @@ class CronController extends Controller
 						$usermininghistory->selisih = $userminingselisih;
 							
 						$usermininghistory->save();
+
+						$user->balance = $user->balance + $userminingselisih;
 					}
 				}
 				
-				/*
-					Pengiriman email jika status TRUE
-				*/
+				/**
+				 * Pengiriman email jika status TRUE
+				 */
 				$emails = [
 					'1' => 'hans@creids.net',
 					'2' => 'anggi@creids.net'
@@ -153,9 +156,9 @@ class CronController extends Controller
 			}
 			else
 			{
-				/*
-					Pengiriman email jika status FALSE
-				*/
+				/**
+				 * Pengiriman email jika status FALSE
+				 */
 				$emails = [
 					'1' => 'hans@creids.net',
 					'2' => 'anggi@creids.net'
